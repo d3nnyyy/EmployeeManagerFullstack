8014a267f86220df7356057c01c70b191097407e
d3nnyyy <d3nnn41k@gmail.com>
2023-06-28 11:59:35 +0300

    Added model

---
+++ EmployeeManagerBackend/src/main/java/ua/tsebulia/EmployeeManagerBackend/model/Employee.java
@@ -0,0 +1,27 @@
+package ua.tsebulia.EmployeeManagerBackend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;
+
+}

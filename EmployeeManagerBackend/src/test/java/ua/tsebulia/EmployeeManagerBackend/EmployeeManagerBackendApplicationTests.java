788d346d39a5d5bc4177dde2323aab5f30fe9d0f
d3nnyyy <d3nnn41k@gmail.com>
2023-06-28 11:55:37 +0300

    Initial backend commit

---
+++ EmployeeManagerBackend/src/test/java/ua/tsebulia/EmployeeManagerBackend/EmployeeManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.tsebulia.EmployeeManagerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
